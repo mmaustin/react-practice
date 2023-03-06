d220d95185ad7f3411a810cf76ad5540d855e998
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-03-06 13:51:55 -0500

    completed the form

--- client/src/components/LocalForm.js
+++ client/src/components/LocalForm.js
@@ -9,23 +9,38 @@ const LocalForm = () => {
 
     const [values, setValues] = useState(userObject)
 
-    const onSetValues = (e) => setValues({[e.target.name]: [e.target.value]})
+    const onSetValues = (e) => setValues({...values, [e.target.name]: e.target.value})
 
     const onHandleSubmit = (e) =>{
         e.preventDefault();
-        console.log(userObject);
+        console.log(`${values.name} : ${values.email} : ${values.password}`);
     }
-    console.log(values);
+    
 
   return (
     <div>
         <form onSubmit={onHandleSubmit}>
             <input
             type="text"
-            name={userObject.name}
-            value={userObject.name}
+            name="name"
+            value={values.name}
             onChange={onSetValues}
             />
+            <input
+            type="email"
+            name="email"
+            value={values.email}
+            onChange={onSetValues}
+            />
+            <input
+            type="password"
+            name="password"
+            value={values.password}
+            onChange={onSetValues}
+            />
+        <p>
+            <button type="submit">Submit</button>
+        </p>
         </form>
     </div>
   )
