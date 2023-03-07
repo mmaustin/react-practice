dd57eba1d63509b55588a6f4a2bedcab25396fd0
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-03-07 12:46:11 -0500

    installed nodemon and changed script in pkg.json

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('What it do!');
+})
+
+const port = 5001
+
+const start = async () => {
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}....`)
+    })
+}
+
+start()
