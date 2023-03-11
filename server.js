39d4ee7e98accc1b9bcdb760f43671abc97be77f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-03-11 16:10:12 -0500

    imported auth router to server file and implemented with app use

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 import express from 'express';
 const app = express();
-import { tryRequest, tryResponse } from './middleware/index.js';
+//import { tryRequest, tryResponse } from './middleware/index.js';
 
 //you can put your response in a middleware, if need be.
 //Ahh, this is exactly what nonfound middleware does, geez
 //app.use(tryResponse);
-app.use(tryRequest);
+//app.use(tryRequest);
 
-app.get('/', (req, res) => {
-  const bp = req.bloodPressure;
-  res.send(bp);
-})
+import authRouter from './routes/authRoutes.js';
+
+app.use(express.json());
+
+app.use('/api/v1/auth', authRouter);
+
+// app.get('/', (req, res) => {
+//   const bp = req.bloodPressure;
+//   res.send(bp);
+// })
 
 const port = 5001
 
