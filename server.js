ebdee2d5508a2d2f330f00638cff2744ca36aaa0
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-03-16 09:32:52 -0400

    imported cors to server file and applied

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 import express from 'express';
 const app = express();
-//import { tryRequest, tryResponse } from './middleware/index.js';
+import { tryRequest, tryResponse } from './middleware/index.js';
 
 //you can put your response in a middleware, if need be.
 //Ahh, this is exactly what nonfound middleware does, geez
-//app.use(tryResponse);
-//app.use(tryRequest);
+app.use(tryResponse);
+app.use(tryRequest);
+import cors from 'cors';
 
-import authRouter from './routes/authRoutes.js';
+//import authRouter from './routes/authRoutes.js';
 
+app.use(cors());
 app.use(express.json());
 
-app.use('/api/v1/auth', authRouter);
+//app.use('/api/v1/auth', authRouter);
 
-// app.get('/', (req, res) => {
-//   const bp = req.bloodPressure;
-//   res.send(bp);
-// })
+app.get('/', (req, res) => {
+  const bp = req.bloodPressure;
+  res.json({bp: bp});
+})
 
 const port = 5001
 
