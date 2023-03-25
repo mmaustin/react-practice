f32e53c4ed0ffc6ed5101fb1ef380e523f4c4eac
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-03-25 12:38:46 -0400

    implemented bare minimum register controller function

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,27 @@
-//dummy commit
-export const register = (req, res) => {
-    res.json({msg: 'I am registered'});
+import User from "../models/User.js";
+
+export const register = async(req, res) => {
+    const { name, email, password } = req.body
+
+    // if (!name || !email || !password) {
+    //   throw new BadRequestError('please provide all values')
+    // }
+    // const userAlreadyExists = await User.findOne({ email })
+    // if (userAlreadyExists) {
+    //   throw new BadRequestError('Email already in use')
+    // }
+    const user = await User.create({ name, email, password })
+  
+    //const token = user.createJWT()
+    res.status(201).json({
+      user: {
+        email: user.email,
+        location: user.location,
+        name: user.name,
+      },
+      location: user.location,
+    })    
+    //res.json({msg: 'I am registered'});
 }
 
 export const login = (req, res) => {
