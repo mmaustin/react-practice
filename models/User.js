18cf28f9ef9d899be3d30b6369aa240444f1aa0f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-03-25 12:13:40 -0400

    added user model properties

--- models/User.js
+++ models/User.js
@@ -1,7 +1,34 @@
 import mongoose from "mongoose";
 
 const UserSchema= mongoose.Schema({
-
+    name: {
+        type: String,
+        required: [true, 'Please provide name'],
+        minlength: 3,
+        maxlength: 20,
+        trim: true,
+      },
+    email: {
+        type: String,
+        required: [true, 'Please provide email'],
+        validate: {
+          validator: validator.isEmail,
+          message: 'Please provide a valid email',
+        },
+        unique: true,
+      },
+    password: {
+        type: String,
+        required: [true, 'Please provide password'],
+        minlength: 6,
+        select: false,
+      },
+    location: {
+        type: String,
+        trim: true,
+        maxlength: 20,
+        default: 'my city',
+      },
 });
 
 export default mongoose.model('User', UserSchema);
