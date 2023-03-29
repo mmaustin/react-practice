809503f47afa005dfe4e0b5e43747127206959cf
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-03-29 11:50:50 -0400

    imported and applied plantation router in server file

--- server.js
+++ server.js
@@ -12,11 +12,13 @@ dotenv.config();
 //import cors from 'cors';
 
 import authRouter from './routes/authRoutes.js';
+import plantationRouter from './routes/plantationRoutes.js'
 
 //app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/plantation', plantationRouter);
 
 // app.get('/cuz', (req, res) => {
 //   res.json({msg: 'i guess they fixed the manhole cover!'});
