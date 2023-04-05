5b632d73ee620916d2d1c0da283beadd8ba233d2
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-04-05 15:18:46 -0400

    installed axios on frontend

--- client/src/components/Manifest.js
+++ client/src/components/Manifest.js
@@ -4,7 +4,7 @@ const Manifest = () => {
 
     const fetchManifest = async() => {
         try {
-            const response = await fetch('https://digitalcollections.nyhistory.org/islandora/object/islandora%3A139196#page/1/mode/1up', {mode: 'no-cors'})
+            const response = await fetch('https://espn.com', {mode: 'no-cors'})
             console.log(response);
             const data = await response.json()
             console.log(data)
