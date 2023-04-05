cf7dc21822893015ed6a908281da314140001573
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-04-05 12:56:21 -0400

    attempting to grab slave ship brig uneas data from the smithsonian

--- client/src/pages/Manifest.js
+++ client/src/pages/Manifest.js
@@ -1,9 +1,22 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
+import {log} from 'console';
 
 
 const Manifest = () => {
 
-    
+    const fetchManifest = async() => {
+        try {
+            const response = await fetch('https://ids.si.edu/ids/manifest/NMAAHC-2013_46_6_001');
+            const data = await response.json();
+            log(data)
+        } catch (error) {
+            log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchManifest();
+    }, [])
 
   return (
     <div>Manifest</div>
