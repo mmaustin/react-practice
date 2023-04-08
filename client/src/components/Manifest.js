28c38fd63db6177c0464fae6b1ba9a75a4d0dd12
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-04-08 13:22:44 -0400

    tested appearance of a slave ship's manifest document

--- client/src/components/Manifest.js
+++ client/src/components/Manifest.js
@@ -1,15 +1,16 @@
 import { useEffect } from "react";
 import axios from 'axios';
 
+
 const Manifest = () => {
 
     const fetchManifest = async() => {
         try {
-            const {response} = await axios.get('https://espn.com', {mode: 'no-cors'})
-            console.log(response);
+            const {data} = await axios.get('https://ids.si.edu/ids/manifest/NMAAHC-2013_46_6_001')
+            //console.log(response);
             //const data = await response.json()
-            // const {data} = response
-            // console.log(data)
+            const {metadata} = data;
+            console.log(metadata)
         } catch (error) {
             console.log(error)
         }
@@ -20,7 +21,9 @@ const Manifest = () => {
     }, [])
 
   return (
-    <div>Manifest</div>
+    <div>
+      <img src="https://digitalcollections.nyhistory.org/iiif/2/islandora%3A139297~JP2~b6e8d076055a2676a2096410641702dfb5d9e5e0245ae63daba1b402c646b023/full/pct:50/0/default.jpg" alt="death boat" />
+    </div>
   )
 }
 export default Manifest
