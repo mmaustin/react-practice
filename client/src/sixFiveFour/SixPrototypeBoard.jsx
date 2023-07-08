074b386a93b38fcb9739268664f40be9313068a6
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-07-08 17:07:42 -0400

    added handlesetcolor functions on box elements to change color to black on click

--- client/src/sixFiveFour/SixPrototypeBoard.jsx
+++ client/src/sixFiveFour/SixPrototypeBoard.jsx
@@ -1,14 +1,30 @@
 import { Box, Stack } from "@mui/material";
+import { useState } from "react";
 
 const SixPrototypeBoard = () => {
+
+  const [colorOne, setColorOne] = useState('orange');
+  console.log(colorOne);
+  const handleSetColorOne = () => setColorOne('black');
+  const [colorTwo, setColorTwo] = useState('pink');
+  const handleSetColorTwo = () => setColorTwo('black');
+  const [colorThree, setColorThree] = useState('red');
+  const handleSetColorThree = () => setColorThree('black');
+  const [colorFour, setColorFour] = useState('blue');
+  const handleSetColorFour = () => setColorFour('black');
+  const [colorFive, setColorFive] = useState('gray');
+  const handleSetColorFive = () => setColorFive('black');
+  const [colorSix, setColorSix] = useState('green');
+  const handleSetColorSix = () => setColorSix('black');
+
   return (
     <Stack direction={'row'}>
-      <Box height={'245px'} width={'80px'} sx={{border: '1px solid blue', bgcolor: 'yellow'}}>ONE</Box>
-      <Box height={'245px'} width={'80px'} sx={{border: '1px solid blue', bgcolor: 'red'}}>TWO</Box>
-      <Box height={'245px'} width={'80px'} sx={{border: '1px solid blue', bgcolor: 'pink'}}>THREE</Box>
-      <Box height={'245px'} width={'80px'} sx={{border: '1px solid blue', bgcolor: 'green'}}>FOUR</Box>
-      <Box height={'245px'} width={'80px'} sx={{border: '1px solid blue', bgcolor: 'blue'}}>FIVE</Box>
-      <Box height={'245px'} width={'80px'} sx={{border: '1px solid blue', bgcolor: 'orange'}}>SIX</Box>
+      <Box onClick={handleSetColorOne} color={'white'} bgcolor={colorOne} height={'245px'} width={'80px'} sx={{border: '1px solid blue' }}>ONE</Box>
+      <Box onClick={handleSetColorTwo} color={'white'} bgcolor={colorTwo} height={'245px'} width={'80px'} sx={{border: '1px solid blue'}}>TWO</Box>
+      <Box onClick={handleSetColorThree} color={'white'} bgcolor={colorThree} height={'245px'} width={'80px'} sx={{border: '1px solid blue'}}>THREE</Box>
+      <Box onClick={handleSetColorFour} color={'white'} bgcolor={colorFour} height={'245px'} width={'80px'} sx={{border: '1px solid blue'}}>FOUR</Box>
+      <Box onClick={handleSetColorFive} color={'white'} bgcolor={colorFive} height={'245px'} width={'80px'} sx={{border: '1px solid blue'}}>FIVE</Box>
+      <Box onClick={handleSetColorSix} color={'white'} bgcolor={colorSix} height={'245px'} width={'80px'} sx={{border: '1px solid blue'}}>SIX</Box>
     </Stack>
   )
 }
