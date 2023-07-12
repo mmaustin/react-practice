0cb58cbf0d3fe47cbd31ac3c15f7edfe84597a54
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-07-12 15:01:59 -0400

    created test questions and answers object

--- client/src/sixFiveFour/numbersObject.js
+++ client/src/sixFiveFour/numbersObject.js
@@ -1,3 +1,30 @@
 
 
 export const objectOfNumbers = {'ONE': 'ONE', 'TWO': 'TWO', 'THREE': 'THREE', 'FOUR': 'FOUR', 'FIVE': 'FIVE', 'SIX': 'SIX'};
+
+export const questionsAnswers = {
+  q1: {question: "fake question 1",
+  answers: ['fake answer 1', 'fake answer 2', 'fake answer 3'],
+  answer: 'fake answer 1'
+  },
+  q2: {question: "fake question 2",
+  answers: ['fake answer 1', 'fake answer 2', 'fake answer 3'],
+  answer: 'fake answer 2'
+  },
+  q3: {question: "fake question 3",
+  answers: ['fake answer 1', 'fake answer 2', 'fake answer 3'],
+  answer: 'fake answer 3'
+  },
+  q4: {question: "fake question 4",
+  answers: ['fake answer 1', 'fake answer 2', 'fake answer 3'],
+  answer: 'fake answer 1'
+  },
+  q5: {question: "fake question 5",
+  answers: ['fake answer 1', 'fake answer 2', 'fake answer 3'],
+  answer: 'fake answer 2'
+  },
+  q6: {question: "fake question 6",
+  answers: ['fake answer 1', 'fake answer 2', 'fake answer 3'],
+  answer: 'fake answer 3'
+  },
+}
