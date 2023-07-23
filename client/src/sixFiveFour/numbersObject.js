96f4a3e1c69be706592d96b5d8f15c316590568e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-07-23 17:27:14 -0400

    added more questions to quiz

--- client/src/sixFiveFour/numbersObject.js
+++ client/src/sixFiveFour/numbersObject.js
@@ -31,15 +31,15 @@ export const questionsAnswers = {
   answers: ['James Monroe', 'George Mason', 'Patrick Henry'],
   answer: 'Patrick Henry'
   },
-  q7: {question: "Franklin, Tennessee is named for this man who was partner in the largest internal slave trading firm in the country.",
+  q7: {question: "Franklin, Tennessee is named for this man who was partner in the largest internal slave trading firm in the country:",
   answers: ['Benjamin Franklin', 'Franklin Templeton', 'Isaac Franklin'],
   answer: 'Isaac Franklin'
   },
-  q8: {question: "Under Lord Dunmore's command, the British Fleet unloaded on and set ablaze this coastal Colonial city.",
+  q8: {question: "Under Lord Dunmore's command, the British Fleet unloaded on and set ablaze this coastal Colonial city:",
   answers: ['Norfolk', 'Baltimore', 'Jersey City'],
   answer: 'Norfolk'
   },
-  q9: {question: "This famed civil rights leader passed away on July 17, 2020, the same day that his colleague in the struggle, John Robert Lewis, passed.",
+  q9: {question: "This famed civil rights leader passed away on July 17, 2020, the same day that his colleague in the struggle, John Robert Lewis, passed:",
   answers: ['Hosea Lorenzo Williams', 'Cordy Tindell Vivian', 'Joseph Echols Lowery'],
   answer: 'Cordy Tindell Vivian'
   },
@@ -51,11 +51,11 @@ export const questionsAnswers = {
   answers: ['500,000', '1,000,000', '1,500,00'],
   answer: '500,000'
   },
-  q12: {question: "Confederate General Jubal Early's command and the Shenandoah Valley both found ruin at the hands of this Union general.",
+  q12: {question: "Confederate General Jubal Early's command and the Shenandoah Valley both found ruin at the hands of this Union general:",
   answers: ['John Pope', 'Benjamin Butler', 'Philip Sheridan'],
   answer: 'Philip Sheridan'
   },
-  q13: {question: "Contrary to initial reports, President Harry Truman did indeed defeat this man in the 1948 presidential contest.",
+  q13: {question: "Contrary to initial reports, President Harry Truman did indeed defeat this man in the 1948 presidential contest:",
   answers: ['Thomas Dewey', 'Wendell Willkie', 'Adlai Stevenson II'],
   answer: 'Thomas Dewey'
   },
@@ -63,8 +63,28 @@ export const questionsAnswers = {
   answers: ['John Wilkes Booth', 'Stephan Douglas', 'Roger Taney'],
   answer: 'Roger Taney'
   },
-  q15: {question: "Serving symbolically for just twenty-four hours, she, nevertheless, was the first woman to serve in the Senate.",
+  q15: {question: "Serving symbolically for just twenty-four hours, she, nevertheless, was the first woman to serve in the Senate:",
   answers: ['Rebecca Latimer Felton', 'Tallulah Bankhead', 'Hattie Caraway'],
   answer: 'Rebecca Latimer Felton'
-  }
+  },
+  q16: {question: "Escorted by President Lyndon Johnson on January 1, 1963, Gerri Wittington, a Black lady, attended a ball at this segregated club:",
+  answers: ['Augusta National Golf Club', 'University of Texas Faculty Club', 'Friars Club'],
+  answer: 'University of Texas Faculty Club'
+  },
+  q17: {question: "The son of a Cherokee mother and Scottish father, this man was Principal Chief when the Jackson Administration ethnically cleansed Cherokee lands:",
+  answers: ['Major Ridge', 'John Ross', 'Pathkiller'],
+  answer: 'John Ross'
+  },
+  q18: {question: "While living in Paris, this future president of his country, joined a group named Groupe des Patriotes Annamites:",
+  answers: ['Ho Chi Minh', 'Kwame Nkrumah', 'Chaim Weizmann'],
+  answer: 'Ho Chi Minh'
+  },
+  q19: {question: "James Madison Hemmings, one of Thomas Jefferson's Black sons, said that this lady suggested that his mother, Sarah(Sally), name him as she did:",
+  answers: ['Martha Washington', 'Elizabeth Fitzhugh', 'Dolly Madison'],
+  answer: 'Dolly Madison'
+  },
+  q20: {question: "This state's constitution served as a model for the U.S. Constitution:",
+  answers: ['Virginia', 'Massachusetts', 'Pennsylvania'],
+  answer: 'Massachusetts'
+  },
 };
